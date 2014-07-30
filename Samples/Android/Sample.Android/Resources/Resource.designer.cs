ee5b12cdc9c1ff53d59cab39575b746d788762e2
Redth <jondick@gmail.com>
2014-07-30 14:03:54 -0400

    Added Android sample showing how to use the fragment to scan

--- Samples/Android/Sample.Android/Resources/Resource.designer.cs
+++ Samples/Android/Sample.Android/Resources/Resource.designer.cs
@@ -66,32 +66,38 @@ namespace Sample.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int buttonScanCustomView = 2131034113;
-			
-			// aapt resource value: 0x7f050000
-			public const int buttonScanDefaultView = 2131034112;
-			
-			// aapt resource value: 0x7f050007
-			public const int buttonZxingFlash = 2131034119;
-			
-			// aapt resource value: 0x7f050008
-			public const int contentFrame = 2131034120;
-			
-			// aapt resource value: 0x7f050002
-			public const int linearLayout1 = 2131034114;
-			
 			// aapt resource value: 0x7f050005
-			public const int linearLayout2 = 2131034117;
-			
-			// aapt resource value: 0x7f050003
-			public const int textView1 = 2131034115;
+			public const int buttonFragment = 2131034117;
 			
 			// aapt resource value: 0x7f050004
-			public const int textView2 = 2131034116;
+			public const int buttonScanCustomView = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int buttonScanDefaultView = 2131034115;
+			
+			// aapt resource value: 0x7f050009
+			public const int buttonZxingFlash = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int contentFrame = 2131034122;
+			
+			// aapt resource value: 0x7f050001
+			public const int fragment_container = 2131034113;
 			
 			// aapt resource value: 0x7f050006
-			public const int textView3 = 2131034118;
+			public const int linearLayout1 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int linearLayout2 = 2131034119;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
+			
+			// aapt resource value: 0x7f050008
+			public const int textView3 = 2131034120;
 			
 			static Id()
 			{
@@ -107,16 +113,19 @@ namespace Sample.Android
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int FragmentActivity = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int ZxingOverlay = 2130903041;
+			public const int Main = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int zxingscanneractivitylayout = 2130903042;
+			public const int ZxingOverlay = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int zxingscannerfragmentlayout = 2130903043;
+			public const int zxingscanneractivitylayout = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int zxingscannerfragmentlayout = 2130903044;
 			
 			static Layout()
 			{
