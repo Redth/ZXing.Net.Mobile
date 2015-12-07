bbcf217040a54e668d1f91b150003103f7f7df36
Redth <jondick@gmail.com>
2015-12-07 14:19:41 -0500

    Added initial Forms work

---
+++ Samples/FormsSample/FormsSample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace FormsSample
+{
+    public class App : Application
+    {
+        ZXingScannerView zxing;
+
+        public App ()
+        {
+            zxing = new ZXingScannerView ();
+
+            // The root page of your application
+            MainPage = new ContentPage {
+                Content = new StackLayout {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        zxing
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart ()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep ()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume ()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
+
---
+++ src/ZXing.Net.Mobile.Forms/ZXingScannerView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ZXing.Net.Mobile.Forms
+{
+    public class ZXingScannerView : View
+    {
+        public ZXingScannerView ()
+        {
+            //IsClippedToBounds = true;
+            VerticalOptions = LayoutOptions.FillAndExpand;
+            HorizontalOptions = LayoutOptions.FillAndExpand;
+        }
+    }
+}
+
