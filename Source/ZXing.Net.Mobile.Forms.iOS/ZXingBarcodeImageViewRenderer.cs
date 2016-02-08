27b71560ed7854754f2563304ec8ad239d115dcf
Redth <jondick@gmail.com>
2016-02-08 10:06:50 -0500

    Regenerate barcode when a property value changes for #233

--- Source/ZXing.Net.Mobile.Forms.iOS/ZXingBarcodeImageViewRenderer.cs
+++ Source/ZXing.Net.Mobile.Forms.iOS/ZXingBarcodeImageViewRenderer.cs
@@ -24,6 +24,13 @@ namespace ZXing.Net.Mobile.Forms.iOS
         ZXingBarcodeImageView formsView;
         UIImageView imageView;
 
+        protected override void OnElementPropertyChanged (object sender, PropertyChangedEventArgs e)
+        {
+            regenerate ();
+
+            base.OnElementPropertyChanged (sender, e);
+        }
+
         protected override async void OnElementChanged(ElementChangedEventArgs<ZXingBarcodeImageView> e)
         {
             formsView = Element;
@@ -35,6 +42,13 @@ namespace ZXing.Net.Mobile.Forms.iOS
                 base.SetNativeControl (imageView);     
             }
 
+            regenerate ();
+
+            base.OnElementChanged (e);
+        }
+
+        void regenerate ()
+        {
             if (formsView != null && formsView.BarcodeValue != null)
             {
                 var writer = new ZXing.Mobile.BarcodeWriter();
@@ -50,8 +64,6 @@ namespace ZXing.Net.Mobile.Forms.iOS
 
                 imageView.Image = image;
             }
-
-            base.OnElementChanged (e);
         }
     }
 }
