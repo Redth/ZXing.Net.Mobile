28fbf65e178665b0f5977fa5da7d7e84edbde50f
Redth <jondick@gmail.com>
2017-04-12 14:12:19 -0400

    Switch to FastAndroidCamera
    This switches the preview buffer handling to the approach taken by FastAndroidCamera.  This means we aren’t marshalling data back and forth for every preview frame.  This is much more efficient for memory usage, and generally a lot faster.

--- Source/ZXing.Net.Mobile.Android/CameraAccess/CameraEventsListener.cs
+++ Source/ZXing.Net.Mobile.Android/CameraAccess/CameraEventsListener.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using Android.Hardware;
+using ApxLabs.FastAndroidCamera;
 
 namespace ZXing.Mobile.CameraAccess
 {
-    public class CameraEventsListener : Java.Lang.Object, Camera.IPreviewCallback, Camera.IAutoFocusCallback
+    public class CameraEventsListener : Java.Lang.Object, INonMarshalingPreviewCallback, Camera.IAutoFocusCallback
     {
-        public event EventHandler<byte[]> OnPreviewFrameReady; 
+        public event EventHandler<FastJavaByteArray> OnPreviewFrameReady; 
 
-        public void OnPreviewFrame(byte[] data, Camera camera)
+        //public void OnPreviewFrame(byte[] data, Camera camera)
+        //{
+        //    OnPreviewFrameReady?.Invoke(this, data);            
+        //}
+
+        public void OnPreviewFrame(IntPtr data, Camera camera)
         {
-            OnPreviewFrameReady?.Invoke(this, data);            
+			using (var fastArray = new FastJavaByteArray(data))
+			{
+				OnPreviewFrameReady?.Invoke(this, fastArray);
+
+				camera.AddCallbackBuffer(fastArray);
+			}
         }
 
         public void OnAutoFocus(bool success, Camera camera)
