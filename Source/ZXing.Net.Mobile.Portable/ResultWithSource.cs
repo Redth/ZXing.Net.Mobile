2e80be0e8d9e1bad9ffa87ffbaffe2d902d6c676
Mikko Hytti <mikko.hytti@fi.fujitsu.com>
2018-09-10 11:29:10 +0300

    ResultWithSource added

---
+++ Source/ZXing.Net.Mobile.Portable/ResultWithSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZXing.Mobile
+{
+    public class ResultWithSource
+    {
+        public ZXing.Result ZXing_Result { get; private set; }
+        public LuminanceSource luminanceSource { get; private set; }
+
+        public ResultWithSource(ZXing.Result ZXing_Result, LuminanceSource luminanceSource)
+        {
+            this.ZXing_Result = ZXing_Result;
+            this.luminanceSource = luminanceSource;
+        }
+    }
+}
