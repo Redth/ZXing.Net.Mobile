78e7659127b2df145954b23c324809a1a0b24535
Michał Mierzyński <mierzynski.michal@gmail.com>
2019-01-14 23:21:18 +0100

    Added support for XF GTK backend

---
+++ Samples/Forms/GTK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+using Application = Gtk.Application;
+
+namespace FormsSample.GTK
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            Forms.Init();
+            ZXing.Net.Mobile.Forms.GTK.Platform.Init ();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("GTK example");
+            window.Show();
+            Application.Run();
+        }
+    }
+}
---
+++ Source/ZXing.Net.Mobile.Forms.GTK/Platform.cs
@@ -0,0 +1,10 @@
+namespace ZXing.Net.Mobile.Forms.GTK
+{
+    public class Platform
+    {
+        public static void Init ()
+        {
+            ZXing.Net.Mobile.Forms.GTK.ZXingBarcodeImageViewRenderer.Init();
+        }
+    }
+}
