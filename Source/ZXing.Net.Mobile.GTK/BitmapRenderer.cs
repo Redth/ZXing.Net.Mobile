08e171859385e2c09cacd2dfd56c6da3fc0a6c96
Andres G. Aragoneses <knocte@gmail.com>
2019-01-19 23:17:34 +0800

    GTK: dispose cairo objs earlier
    Otherwise a warning is printed in the console.

--- Source/ZXing.Net.Mobile.GTK/BitmapRenderer.cs
+++ Source/ZXing.Net.Mobile.GTK/BitmapRenderer.cs
@@ -11,25 +11,30 @@ namespace ZXing.Net.Mobile.GTK
         {
             var black = new Cairo.Color(0, 0, 0);
             var white = new Cairo.Color(1, 1, 1);
-            var surface = new ImageSurface(Format.RGB24, matrix.Width, matrix.Height);
-            var cr = new Context(surface);
-            for (var x = 0; x < matrix.Width; x++)
+            using (var surface = new ImageSurface(Format.RGB24, matrix.Width, matrix.Height))
             {
-                for (var y = 0; y < matrix.Height; y++)
+                using (var cr = new Context(surface))
                 {
-                    SetSourceColor(cr, matrix[x, y] ? black : white);
-                    cr.MoveTo(x, y);
-                    cr.LineTo(x + 1, y);
-                    cr.Stroke();
+                    for (var x = 0; x < matrix.Width; x++)
+                    {
+                        for (var y = 0; y < matrix.Height; y++)
+                        {
+                            SetSourceColor(cr, matrix[x, y] ? black : white);
+                            cr.MoveTo(x, y);
+                            cr.LineTo(x + 1, y);
+                            cr.Stroke();
+                        }
+                    }
+                    return new Pixbuf(surface.Data,
+                        Colorspace.Rgb,
+                        true,
+                        8,
+                        matrix.Width,
+                        matrix.Height,
+                        surface.Stride);
                 }
             }
-            return new Pixbuf(surface.Data,
-                Colorspace.Rgb,
-                true,
-                8,
-                matrix.Width,
-                matrix.Height,
-                surface.Stride);
+
         }
 
         private void SetSourceColor(Context context, Cairo.Color color)
