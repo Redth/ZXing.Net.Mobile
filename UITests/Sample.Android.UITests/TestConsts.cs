8e404c87a4cbfe1e3f7250c5790ff969f6a1155f
Jonathan Dick <jondick@gmail.com>
2020-03-11 20:21:25 -0400

    Remove old tests (we'll start over)

--- UITests/Sample.Android.UITests/TestConsts.cs
+++
@@ -1,7 +0,0 @@
-namespace UITests
-{
-    public static class TestConsts
-    {
-        public const string ApkFile = "../../../../Samples/Android/Sample.Android/bin/Release/com.altusapps.zxingnetmobile.apk";
-    }
-}
