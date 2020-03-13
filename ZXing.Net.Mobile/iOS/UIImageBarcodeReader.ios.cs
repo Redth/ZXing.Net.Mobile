571b132893fe3241f0e50cbd263b0dcd05a90f19
Jonathan Dick <jondick@gmail.com>
2020-03-13 09:01:58 -0400

    Add a UIImageBarcodeReader for convenience

---
+++ ZXing.Net.Mobile/iOS/UIImageBarcodeReader.ios.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using ZXing.Mobile;
+
+namespace ZXing.Mobile
+{
+	public class UIImageBarcodeReader : BarcodeReader<UIImage>, IBarcodeReader
+	{
+		static readonly Func<UIImage, LuminanceSource> defaultCreateLuminanceSource =
+		   (img) => new RGBLuminanceSourceiOS(img);
+
+		public UIImageBarcodeReader()
+		   : this(null, defaultCreateLuminanceSource, null)
+		{
+		}
+
+		public UIImageBarcodeReader(Reader reader,
+		   Func<UIImage, LuminanceSource> createLuminanceSource,
+		   Func<LuminanceSource, Binarizer> createBinarizer
+		)
+		   : base(reader, createLuminanceSource ?? defaultCreateLuminanceSource, createBinarizer)
+		{
+		}
+
+		public UIImageBarcodeReader(Reader reader,
+		   Func<UIImage, LuminanceSource> createLuminanceSource,
+		   Func<LuminanceSource, Binarizer> createBinarizer,
+		   Func<byte[], int, int, RGBLuminanceSource.BitmapFormat, LuminanceSource> createRGBLuminanceSource
+		)
+		   : base(reader, createLuminanceSource ?? defaultCreateLuminanceSource, createBinarizer, createRGBLuminanceSource)
+		{
+		}
+	}
+}
