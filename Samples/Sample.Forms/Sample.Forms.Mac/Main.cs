d8c7b9e500f9cb9ee810976c5a518fd3eb1cdfae
Jonathan Dick <jondick@gmail.com>
2020-03-13 20:55:08 -0400

    Added Forms macOS sample

---
+++ Samples/Sample.Forms/Sample.Forms.Mac/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace Sample.Forms.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate(); // add this line
+            NSApplication.Main(args);
+        }
+    }
+}
