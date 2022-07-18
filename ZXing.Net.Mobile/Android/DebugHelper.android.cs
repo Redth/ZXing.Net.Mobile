af54b7f9462dc376ac016b0feb6eac9b456dd410
DarkIrata <git@ipmix.de>
2022-07-18 16:03:16 +0200

    Add DebugHelper.android.cs class

---
+++ ZXing.Net.Mobile/Android/DebugHelper.android.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Net.Http;
+using Android.Graphics;
+
+namespace ZXing.Net.Mobile.Android
+{
+#if DEBUG
+    // Do not use this in production.
+    // This is a simple helping class to use with the ImageReceiver Tool
+    public static class DebugHelper
+    {
+        // Used to check NV21 Output
+        static byte[] NV21toJPEG(byte[] nv21, int width, int height)
+        {
+            using (var mems = new MemoryStream())
+            {
+                var yuv = new YuvImage(nv21, ImageFormatType.Nv21, width, height, null);
+                yuv.CompressToJpeg(new Rect(0, 0, width, height), 100, mems);
+                return mems.ToArray();
+            }
+        }
+
+        public static void SendBytesToEndpoint(byte[] data, string endpoint)
+        {
+            var httpClient = GetHttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, endpoint);
+            request.Content = new ByteArrayContent(data);
+            httpClient.SendAsync(request);
+        }
+
+        public static void SendNV21toJPEGToEndpoint(byte[] data, int width, int height, string endpoint)
+        {
+            var jpeg = NV21toJPEG(data, width, height);
+            SendBytesToEndpoint(jpeg, endpoint);
+        }
+
+        static HttpClient GetHttpClient()
+        {
+            var handler = new HttpClientHandler();
+            // we dont need to validate certificates
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            handler.ServerCertificateCustomValidationCallback = (_, __, ___, ____) => true;
+
+            return new HttpClient(handler);
+        }
+    }
+#endif
+}
